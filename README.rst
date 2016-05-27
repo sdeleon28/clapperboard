0ba8411435210eaff19e02cfd750cd9f0348998e
Santiago de Leon <Santiago de Leon>
2016-05-27 00:40:19 -0300

    First installable version

---
+++ README.rst
@@ -0,0 +1,27 @@
+# Clapperboard
+
+Want record yourself playing guitar with a camcorder while using the audio from
+your audio interface? No problem, we've got you covered!
+
+Clapperboard syncronizes your video an audio by using a clapperboard-like
+technique. If you don't know what I'm talking about, have a look at this post
+-> http://www.learningguitarnow.com/blog/recording/how-to-record-a-video-while-playing-over-a-backing-track/
+
+Instead of using a pricy UI based NLE, you can instead use this handy command
+line script to sync your video and audio tracks.
+
+Installation
+============
+
+Clone this repo and run `pip install clapperboard`.
+
+Usage
+=====
+
+Syntax::
+
+  clapsync audio_filename video_filename video_start audio_start out_filename
+
+Example::
+
+  clapsync guitar_solo.mp4 guitar_solo.wav 15.32 2.45 guitar_solo_sync.mp4
---
+++ clapperboard/sync.py
@@ -0,0 +1,26 @@
+from moviepy.editor import *
+import sys
+import os
+
+
+def sync_clips(video_filename, audio_filename, video_start, audio_start, out_filename):
+	video_clip = VideoFileClip(video_filename, audio=False).subclip(video_start)
+	audio_clip = AudioFileClip(audio_filename).subclip(audio_start)
+	video_clip = video_clip.set_audio(audio_clip)
+	video_clip.write_videofile(out_filename, fps=25)
+
+
+def main(args=None):
+	if args is None:
+        args = sys.argv[1:]
+	video_filename, audio_filename, video_start, audio_start, out_filename = args
+	video_filename = os.path.abspath(video_filename)
+	audio_filename = os.path.abspath(audio_filename)
+	video_start = float(video_start)
+	audio_start = float(audio_start)
+	out_filename = os.path.abspath(out_filename)
+	sync_clips(video_filename, audio_filename, video_start, audio_start, out_filename)
+
+
+if __name__ == '__main__':
+	main()
